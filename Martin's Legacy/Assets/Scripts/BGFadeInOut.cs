1a6f3802a587400c4db09aa0a7821c8d53d54988
JingYi Yuan <2656617231@qq.com>
2018-01-30 14:08:52 +0800

    Add files via upload

--- Martin's Legacy/Assets/Scripts/BGFadeInOut.cs
+++ Martin's Legacy/Assets/Scripts/BGFadeInOut.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿
+//苑靖怡 2018-1-30 
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,13 +9,14 @@ using UnityEngine.SceneManagement;
 
 public class BGFadeInOut : MonoBehaviour {
 
+	//currentTime记录当前时间、showTime记录出现时间、fadeTime记录隐藏时间
 	public Image presentImage;
 	public float currentTime;
 	public float showTime = 3;
 	public float fadeTime = 2;
 	//private bool show = true ;
 
-	// Use this for initialization
+	//初始化设置界面α通道为0
 	void Start () {
 		presentImage.color = new Color(1, 1, 1, 0);
 	}
@@ -21,35 +25,23 @@ public class BGFadeInOut : MonoBehaviour {
 	void Update () {
 		currentTime += Time.deltaTime;
 
+		//若当前时间在出现时间段
 		if (currentTime >= 0 && currentTime < showTime) 
 		{
 			presentImage.color = new Color (1, 1, 1, (currentTime / showTime));
 		} 
+		//若当前时间在隐藏时间段
 		else if (currentTime >= showTime && currentTime < showTime+fadeTime) 
 		{
 			presentImage.color = new Color (1, 1, 1, 1 - ((currentTime-showTime)/ fadeTime));
 		}
+		//否则跳转页面
 		else {
 			loadLoadingScene ();
 		}
-//		if (ShowTimeTrigger > showTime)
-//		{
-//			if (fadeTimeTrigger >= 0 && fadeTimeTrigger < fadeTime)
-//			{
-//				fadeTimeTrigger += Time.deltaTime;
-//				if(show)
-//				{
-//					bgimages.color = new Color(1, 1, 1, 1 - (fadeTimeTrigger / fadeTime));
-//
-//				}
-//				else
-//				{
-//					bgimages.color = new Color(1, 1, 1, (fadeTimeTrigger / fadeTime));
-//
-//				}
-//			}
-
 	}
+
+	//加载loading页面
 	void loadLoadingScene() {
 		SceneManager.LoadScene ("Loading");
 	}
