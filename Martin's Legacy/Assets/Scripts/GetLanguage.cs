dad29fd4cbb61c1691f2663c5b4dccf4b7278af8
JingYi Yuan <2656617231@qq.com>
2018-02-06 19:33:34 +0800

    Add files via upload

---
+++ Martin's Legacy/Assets/Scripts/GetLanguage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Xml;
+
+public class GetLanguage : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		Debug.Log ("language!");
+		string language = UserDataManager.instance.GetLanguage();
+		GameObject.Find("Language").GetComponent<Text>().text = language;
+	}
+
+}
