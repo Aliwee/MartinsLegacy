0d562dc7aefa2db6dd0d39beee40f7def1d7cfad
AliweeLee <731275785@qq.com>
2018-02-23 20:02:38 +0800

    Add Thanks Scene
    Added a new scene called Thanks. This will be loaded once player has finished demo process.

--- Martin's Legacy/Assets/Scripts/BGFadeInOut.cs
+++ Martin's Legacy/Assets/Scripts/BGFadeInOut.cs
@@ -6,15 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class BGFadeInOut : MonoBehaviour {
 
+	private string levelName;
 
 	public Image presentImage;
-	public float currentTime;        //当前时间
-	public float showTime = 3;       //出现时间
-	public float fadeTime = 2;       //隐藏时间
+	public float currentTime;    //当前时间
+	public float showTime;       //出现时间
+	public float fadeTime;       //隐藏时间
 
 	//设置图片的α通道为0
 	void Start () {
 		presentImage.color = new Color(1, 1, 1, 0);
+		//获取当前场景名
+		levelName = SceneManager.GetActiveScene ().name;
 	}
 	
 	// Update is called once per frame
@@ -33,13 +36,30 @@ public class BGFadeInOut : MonoBehaviour {
 		}
 		//否则跳转页面
 		else {
-			loadLoadingScene ();
+			ChooseSceneToLoad (levelName);
 		}
-
 	}
 
+	//根据当前场景选择下一个场景
+	void ChooseSceneToLoad(string levelName) {
+		switch (levelName) {
+		case "Presents":
+			loadLoadingScene ();
+			break;
+		case "Thanks":
+			LoadStartScene ();
+			break;
+		default :
+			break;
+		}
+	}
 	//加载Loading页面
 	void loadLoadingScene() {
 		SceneManager.LoadScene ("Loading");
+	}
+
+	//加载Thanks页面
+	void LoadStartScene() {
+		SceneManager.LoadScene ("Start");
 	}
 }
